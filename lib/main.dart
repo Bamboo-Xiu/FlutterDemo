554d59ee4145a7bdf391d515ba22bf86f572d3fd
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-01-04 13:04:09 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      //应用名称
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        //蓝色主题
+        primarySwatch: Colors.blue,
+      ),
+      //注册路由表
+      routes: {
+        "new_page":(context)=>NewRoute(),
+      },
+      //应用首页路由
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            FlatButton(
+              child: Text("open new route"),
+              textColor: Colors.blue,
+              onPressed: (){
+                //通过路由名打开新的路由页
+                Navigator.pushNamed(context, "new_page");
+                //导航到新路由
+//                Navigator.push(context,
+//                new MaterialPageRoute(builder: (context){
+//                  return NewRoute();
+//                }));
+              },
+            ),
+            new RandomWordWidget(),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NewRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
+      ),
+      body: Center(
+        child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+class RandomWordWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding:const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
+    );
+
+  }
+}
