a06f6ab4a41587c12a82fc2995cb8fbdc38c21c0
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-01-17 13:29:47 +0800

    widget study

--- lib/NewRoute.dart
+++ lib/NewRoute.dart
@@ -124,7 +124,7 @@ class TapboxB extends StatelessWidget{
  */
 //------------------------ ParentWidget --------------------------------
 class ParentWidgetC extends StatefulWidget{
-
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
 }
 
 class _ParentWidgetCState extends State<ParentWidgetC>{
@@ -138,6 +138,76 @@ class _ParentWidgetCState extends State<ParentWidgetC>{
 
   @override
   Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
 
+//------------------------- TapboxC ----------------------------------
+class TapboxC extends StatefulWidget{
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  TapboxC({Key, key,this.active: false, @required this.onChanged})
+   :super(key:key);
+
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC>{
+  bool _hightlight = false;
+
+  void _handleTapDown(TapDownDetails details){
+    setState(() {
+      _hightlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details){
+    setState(() {
+      _hightlight = false;
+    });
+  }
+
+  void _handleTapCancel(){
+    setState(() {
+      _hightlight = false;
+    });
+  }
+
+  void _handleTap(){
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //在按下时添加绿色边框，当抬起时，取消高亮
+    return new GestureDetector(
+      onTapDown: _handleTapDown,    //处理按下事件
+      onTapUp: _handleTapUp,    //处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(widget.active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 20.0, color: Colors.white),),
+        ),
+        width: 100.0,
+        height: 100.0,
+        decoration: new BoxDecoration(
+          color:widget.active ? Colors.lightBlue : Colors.grey,
+          border: _hightlight
+            ? new Border.all(
+            color: Colors.teal[700],
+            width: 5.0
+          )
+              : null,
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
             new RandomWordsWidget(),
 //            new Echo(text:"hiahiahiahia~")
             new TapboxA(),
-            new ParentWidget(),
+//            new ParentWidget(),
+            new ParentWidgetC()
           ],
         ),
       ),
