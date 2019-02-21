905412f69c170977db83bb290756f647b20fac0d
wenjinchao01 <wenjinchao01@daojia-inc.com>
2019-02-21 14:34:18 +0800

    study the DecoratedBox optimize the code

--- lib/PaddingTestRoute.dart
+++ lib/PaddingTestRoute.dart
@@ -3,47 +3,55 @@ import 'package:flutter/material.dart';
 class PaddingTestRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      //上下左右各添加16像素补白
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        //显式指定对齐方式为左对齐，排除对齐干扰
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            //左边添加8像素补白
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text("Hello world"),
-          ),
-          Padding(
-            //上下各添加8像素补白
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text("I am Jack"),
-          ),
-          Padding(
-            // 分别指定四个方向的补白
-            padding: const EdgeInsets.fromLTRB(20.0,.0,20.0,20.0),
-            child: Text("Your friend"),
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Colors.red,Colors.orange[700]]),
-              borderRadius: BorderRadius.circular(4.0),    // 4像素圆角
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54,
-                  offset: Offset(2.0, 2.0),
-                  blurRadius: 4.0
-                )
-              ]
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: Text("Padding and DecoratedBox"),
+
+        ),
+        body: Padding(
+        //上下左右各添加16像素补白
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          //显式指定对齐方式为左对齐，排除对齐干扰
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              //左边添加8像素补白
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Hello world"),
             ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 20.0),
-              child: Text("Login",style: TextStyle(color: Colors.white),),
+            Padding(
+              //上下各添加8像素补白
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text("I am Jack"),
             ),
-          )
-        ],
+            Padding(
+              // 分别指定四个方向的补白
+              padding: const EdgeInsets.fromLTRB(20.0,.0,20.0,20.0),
+              child: Text("Your friend"),
+            ),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [Colors.red,Colors.orange[700]]),
+                  borderRadius: BorderRadius.circular(4.0),    // 4像素圆角
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black54,
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 4.0
+                    )
+                  ]
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 20.0),
+                child: Text("Login",style: TextStyle(color: Colors.white),),
+              ),
+            )
+          ],
+        ),
       ),
-    );
+      );
+
   }
 }
